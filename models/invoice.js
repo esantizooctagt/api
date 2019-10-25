07006934e3d961674722c75bf6bd436bccb71826
Erick Santizo <esantizo@octagt.com>
2019-10-25 13:01:15 -0600

    mongodb part 2

--- models/invoice.js
+++ models/invoice.js
@@ -11,7 +11,8 @@ const invoiceSchema = mongoose.Schema({
     Total: { type: Number, required: true },
     Total_Tax: { type: Number, required: true },
     Total_Discount: { type: Number, required: true },
-    User_Id : { type:mongoose.Schema.Types.ObjectId, required: true, ref = 'User' },
+    User_Id: { type: mongoose.Schema.Types.ObjectId, required: true, ref = 'User' },
+    Cashier_Id: { type: mongoose.Schema.Types.ObjectId, required: true, ref = 'Cashier' },
     Company_Id: { type:mongoose.Schema.Types.ObjectId, required: true, ref = 'Company' },
     Customer: {
         Customer_Id: { type:mongoose.Schema.Types.ObjectId, required: true, ref = 'Customer' },
@@ -19,18 +20,37 @@ const invoiceSchema = mongoose.Schema({
         Address : { type: String, required: true },
         House_No: { type: String, required: false },
         Postal_Code: { type: String, required: false },
+        Phone: { type: String, required: false },
+        Email: { type: String, required: false },
         Tax_Number: { type: String, required: true },
-        Status: { type: Number, required:true, Default: 0 },
-        Cashiers: [{
-            Cashier_Id: { type: mongoose.Schema.Types.ObjectId, required: true, ref = 'Cashier' },
-            Description: { type: String, required: true },
-            Status: { type: Number, required:true, Default: 0 }
-        }],
+        Is_Exent: { type: Boolean, required: true, Default: false },
+        Reason: { type: String, required: false },
+        Status: { type: Number, required:true, Default: 1 }
     },
-    Applications: [{
-            Application_Id: { type: mongoose.Schema.Types.ObjectId, required: true, ref = 'Application' }
+    Items: [{
+        Line_No: { type: Number, required: true },
+        Product: {  
+                    Product_Id: { type: mongoose.Schema.Types.ObjectId, required: true, ref = 'Product' }, 
+                    Type_Id: { type: String, required = true },
+                    Img_Path: { type: String, required = false }
+                 },
+        Name: { type: String, required: true },
+        Type: { type: String, required: true },
+        Tax: { 
+                Tax_Id: { type: mongoose.Schema.Types.ObjectId, required: true },
+                Name: { type: String, required: true },
+                Percentage: { type: Number, required: true },
+                Include_Tax: { type: Boolean, required: true } 
+             },
+        Qty: { type: Number, required: true },
+        Unit_Price: { type: Number, required: true },
+        Discount: { type: Number, required: false },
+        Total: { type: Number, required: true },
+        To_Go: { type: Boolean, required: true },
+        Delivery_Date: { type: Date, required: false }
+
     }],
-    Status: { type: Number, required: true, Default: 0 }, 
+    Status: { type: Number, required: true, Default: 1 }, 
     Create_Date: { type: Date, required: true, default: Date.now },
     Modified_Date: Date
 });
